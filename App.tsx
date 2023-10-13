66764bbb942f7f4660f70b4e2d75477c14028d35
Jeremiah Hogue <hogue@Jeremiahs-MacBook-Pro.local>
2023-10-13 17:13:12 -0600

    stashing

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, Pressable, StyleSheet, Text, View } from 'react-native';
+import * as Device from 'expo-device';
 
 export default function App() {
+  console.log('poo')
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>blo</Text>
       <StatusBar style="auto" />
+      <Pressable style={styles.inner} onPress={()=>alert('Fuck')}>
+        <Text style={styles.text}>blahblah</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +18,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+  },
+  inner: {
+    borderColor: '#00FFFF',
+    borderWidth: 2,
+    borderRadius: 5,
+    padding: 5
+  }
 });
